8ff51e4550e56221e3a5a7c1935da54bc6393567
diegowagnermoran <112007947+diegowagnermoran@users.noreply.github.com>
2023-04-06 20:12:20 -0400

    Better late than never i guess

--- actions.js
+++ actions.js
@@ -1,6 +1,49 @@
 /* Write yor code here */
 
-document.addEventListener('DOMContentLoaded', function() {
+// var, const, key
+// one-word, -,_, camelCase
+//123name
+// first name =
+const person = {
+  firstName: 'Diego',
+  lastName: 'Wagner',
+  age: '22',
+  major: 'Media Production'
+
+}
+
+const favoriteBands = [ 
+'100 gecs', 
+'M83', 
+'Sophie']
+
+function  getFullName(person) 
+{
+  return person.firstName + ' ' + person.lastName
+}
+
+function printBiography (person, bands) {
+  console.log('My name is ' + getFullName(person) + ' ' + person.lastName + '. I am ' + person.age + 'years old. I am studying ' + person.major + 'at the University of Cincinnati. My favorite musical acts are:')
+  // bands.forEach (function(band))
+  bands.forEach(function(band) {
+    console.log(band)
+  })
+}
+
+
+/*document.addEventListener('DOMContentLoaded', function() {
+
+// const button = document.querySelector('button')
+
+// button.addEventListener('click', function ()
+// {alertMe()})
+
+  // console.log ('100 gecs')
+  // const fullName = getFullName(person)
+
+  console.log(printBiography(person, favoriteBands))
+
   // Execute your functions here
   // ex: printBiography(person, bands)
 })
+*/
